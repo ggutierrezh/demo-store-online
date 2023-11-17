ae4c1b24d1567dd8b81ed06529c2751a4882de4d
ggutierrezh <germangh2018@gmail.com>
2023-11-17 13:14:13 -0600

    Implementación de Eureka Server y Eureka Client

--- customer-service/src/main/java/mx/demo/store/servicecustomer/CustomerServiceApplication.java
+++ customer-service/src/main/java/mx/demo/store/servicecustomer/CustomerServiceApplication.java
@@ -2,8 +2,10 @@ package mx.demo.store.servicecustomer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
