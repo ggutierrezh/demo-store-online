7a137af4ab8cac3ad28997299c7ec20c6a2fa55a
ggutierrezh <germangh2018@gmail.com>
2023-11-18 21:46:53 -0600

    cambios

--- shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/CustomerServiceClient.java
+++ shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/CustomerServiceClient.java
@@ -4,10 +4,8 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "customer-service")
-@RequestMapping("/customers")
+@FeignClient(name = "customer-service", path = "/customers")
 public interface CustomerServiceClient {
 	
 	@GetMapping
--- shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/ProductServiceClient.java
+++ shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/ProductServiceClient.java
@@ -4,10 +4,8 @@ import java.util.List;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name = "product-service")
-@RequestMapping("/products")
+@FeignClient(name = "product-service", path = "/products")
 public interface ProductServiceClient {
 
 	@GetMapping
