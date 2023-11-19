8d83e41b39a6f2b766a43800ca5424eb43c378ac
ggutierrezh <germangh2018@gmail.com>
2023-11-18 22:00:12 -0600

    Nuevos cambios

--- shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/CustomerServiceClient.java
+++ shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/CustomerServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "customer-service", path = "/customers")
+@FeignClient(name = "customer-service", url ="/customers")
 public interface CustomerServiceClient {
 	
 	@GetMapping
--- shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/ProductServiceClient.java
+++ shopping-service/src/main/java/mx/demo/store/shoppingservice/feing/clients/ProductServiceClient.java
@@ -5,7 +5,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@FeignClient(name = "product-service", path = "/products")
+@FeignClient(name = "product-service", url = "/products")
 public interface ProductServiceClient {
 
 	@GetMapping
