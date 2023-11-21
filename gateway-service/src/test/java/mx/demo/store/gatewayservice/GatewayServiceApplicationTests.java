ab058d2f43419b215c937b08a7ce1ab75ca42a62
ggutierrezh <germangh2018@gmail.com>
2023-11-21 15:49:23 -0600

    Implementación de GateWay

---
+++ gateway-service/src/test/java/mx/demo/store/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.demo.store.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
