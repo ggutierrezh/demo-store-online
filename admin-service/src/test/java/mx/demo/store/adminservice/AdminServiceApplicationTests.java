8a9002b99dbc9b9a041a76ee452f1a4728f073db
ggutierrezh <germangh2018@gmail.com>
2023-11-21 19:27:18 -0600

    Implementación de Admin Server

---
+++ admin-service/src/test/java/mx/demo/store/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.demo.store.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
